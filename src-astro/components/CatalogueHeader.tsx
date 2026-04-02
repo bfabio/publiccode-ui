0a2d5968750608197c2ed4c7f4943da0c272cd7c
Fabio Bonelli <fb@fabiobonelli.it>
2026-04-02 21:21:11 +0200

    feat: port CatalogueItem/Grid/Header, replace Gatsby Link <a>

---
+++ src-astro/components/CatalogueHeader.tsx
@@ -0,0 +1,79 @@
+import React, { useCallback, useContext } from "react";
+import { useTranslation } from "react-i18next";
+import { SearchBar } from "../SearchBar";
+import {
+  searchContextDispatch,
+  searchContextState,
+  setSearchValue,
+} from "../../contexts/searchContext";
+import { Header } from "../Header";
+import { CatalogueFiltersContainer } from "./CatalogueFiltersContainer";
+import { Link } from "gatsby";
+
+export const CatalogueHeader = React.memo(() => {
+  const { searchValue } = useContext(searchContextState);
+  const dispatch = useContext(searchContextDispatch);
+  const { t } = useTranslation();
+
+  const handleSearch = useCallback(
+    (value) => dispatch(setSearchValue(value)),
+    [dispatch]
+  );
+
+  return (
+    <>
+      <Header>
+        <div className="col-12">
+          <SearchBar
+            onChange={handleSearch}
+            defaultValue={searchValue}
+            placeholder={t("search_form_label")}
+          />
+        </div>
+      </Header>
+      <div className="container">
+        {/* breadcrumb */}
+        <div className="row mt-3">
+          <div className="col-1"></div>
+          <div className="mx-auto col-10">
+            <nav
+              className="breadcrumb-container"
+              aria-label="Percorso di navigazione"
+            >
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <a href="/">Home</a>
+                  <span className="separator">/</span>
+                </li>
+                <li className="breadcrumb-item active" aria-current="page">
+                  Software
+                </li>
+              </ol>
+            </nav>
+          </div>
+          <div className="col-1"></div>
+        </div>
+        <div className="row mt-3 mb-5">
+          {/* search terms */}
+          <div className="col-1" />
+          {searchValue && (
+            <div className="col-10">
+              <span className="fs-3">{t('software.search')}: </span>
+              <span className="fw-bold fs-3">{searchValue}</span>
+            </div>
+          )}
+          <div className="col-1" />
+        </div>
+      </div>
+      <div className="row filters-container py-5">
+        <div className="col-lg-1"></div>
+        {/* <div className="col-lg-10"> */}
+        <CatalogueFiltersContainer prefixName="desktop_view" />
+        {/* </div> */}
+        <div className="col-lg-1"></div>
+      </div>
+    </>
+  );
+});
+
+CatalogueHeader.displayName = "CatalogueHeader";
---
+++ src-astro/components/CatalogueItemGrid.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { createUseStyles } from "react-jss";
+import { ImageWithPlaceholder } from "../ImageWithPlaceholder";
+import { SearchType } from "../../utils/proptypes";
+import { useTranslation } from "react-i18next";
+
+import { Icon } from "../Icon/Icon";
+import { TagList } from "../TagList";
+import {absoluteUrl} from "../../utils/publiccodeAbsoluteUrl";
+const useStyles = createUseStyles({
+  link: {
+    display: "block",
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "none",
+    },
+  },
+  logoContainer: {
+    composes: "my-3",
+    height: "150px",
+    width: "100%",
+  },
+  category: {
+    fontSize: "0.8rem",
+    textTransform: "uppercase",
+  },
+  title: {
+    composes: "pt-3 pt-md-0",
+    color: "var(--black)",
+    fontSize: "1.2rem",
+    fontWeight: "bold",
+    textDecoration: "none",
+    wordBreak: "break-word",
+    "&:hover": {
+      textDecoration: "none",
+    },
+  },
+  description: {
+    composes: "mt-3 mb-2",
+    color: "var(--black)",
+    fontSize: "0.9rem",
+    fontWeight: "300",
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "none",
+    },
+  },
+  readMore: {
+    textTransform: "uppercase",
+    fontWeight: "600",
+    marginTop: "auto",
+    marginBottom: "2rem",
+    "&:hover": {
+      textDecoration: "none",
+    },
+  },
+});
+
+export const CatalogueItemGrid: React.FC<SearchType> = ({
+  id,
+  publiccode: { url: repoUrl, description, name, slug, categories, logo },
+}) => {
+  const classes = useStyles();
+  const { t, i18n } = useTranslation();
+
+  const localizedDescription =
+    description[i18n.language] ||
+    description.en ||
+    description[Object.keys(description).find((k) => description[k])];
+  const icon = "it-software";
+  logo ||= description?.screenshots?.[0];
+  const url = `/software/${slug.toLowerCase()}`;
+
+  return (
+    <article
+      id={id}
+      className="d-flex flex-column align-items-start h-100 mb-4 mb-sm-0 px-10"
+      data-testid={id}
+      data-className="catalogue-item"
+    >
+      <div>
+        <Icon icon={icon} size="sm" className="mr-1" />
+      </div>
+      <div className="my-2 my-md-0 w-100">
+        <a
+          href={url}
+          title={name}
+          className={classes.link}
+          data-testid="item-anchor"
+        >
+          {logo && (
+            <div className={classes.logoContainer}>
+              <ImageWithPlaceholder
+                alt="logo"
+                img={absoluteUrl(logo, repoUrl)}
+              />
+            </div>
+          )}
+          <div className={classes.title}>{name}</div>
+          <div className={classes.description}>
+            {localizedDescription.shortDescription}
+          </div>
+        </a>
+      </div>
+      <TagList tags={categories} visibleCount={3} />
+
+      <a href={url} title={name} className={classes.readMore}>
+        {t("readmore")} →
+      </a>
+    </article>
+  );
+};
+
+export default CatalogueItemGrid;
