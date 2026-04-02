e4824836c38573bccda76fe16e611499694b693f
Fabio Bonelli <fb@fabiobonelli.it>
2026-04-02 23:00:53 +0200

    fix: redirect / respects BASE_PATH

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,4 @@ export default defineConfig({
   site: "https://bfabio.github.io",
   base: process.env.BASE_PATH ?? "/",
   integrations: [react()],
-  redirects: {
-    "/": "/software/",
-  },
 });
